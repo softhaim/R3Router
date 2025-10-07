fe7744df6f118da2492d90dae9e5b6053fa1f64b
softhaim <142509212+softhaim@users.noreply.github.com>
2025-10-07 18:48:21 +0900

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -2,8 +2,8 @@
 set -e
 
 python step1.py \
-  --data train_math_500.jsonl \
-  --save_name router_rl_MATH_GRPO.pt \
+  --data train_GSM8K.jsonl \
+  --save_name router_rl_GSM8K_GRPO.pt \
   --models SLM Qwen/Qwen2.5-7B-Instruct meta-llama/Llama-3.1-8B-Instruct Qwen/Qwen2.5-14B-Instruct \
   --rl_alg grpo --group_size 4 --baseline loo --reward_norm group \
   --kl_coef 0.25 --ce_coef 0.10 --ent_coef 0.01
